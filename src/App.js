7f7602f6d55897c61eeec8129b392a1fa88ecd21
hun2zz <tkdgnsdldkdlel@naver.com>
2024-06-12 10:47:04 +0900

    ✅ 동적 데이터 렌더링

--- src/App.js
+++ src/App.js
@@ -1,14 +1,11 @@
-import logo from "./logo.svg";
 import "./App.css";
 import React from "react";
-import Hello from "./components/Hello";
-import Bye from "./components/Bye";
-import Greet from "./components/Greet";
+import ExpenseItem from "./components/expenses/ExpenseItem";
 
-function App() {
+const App = () => {
   // jsx 문법
   // const $h2 = React.createElement("h2", null, "방가방가햄토링~");
-  const $h2 = <h2>방방가햄토리잉</h2>;
+
   // jsx 규칙
   //1. return 안에 있는 태그는 형제 태그가 있을 경우 부모 태그 하나로 반드시 묶어야 함.
   //2. 빈 태그 (닫는 태그가 없는)는 반드시 />로 마감 작업을 해줘야함.
@@ -16,14 +13,12 @@ function App() {
   //4. 의미없는 부모는 <React.Fragment>로 감싸면 됨. -> 축약해서 <> 다이아몬드로 축약 가능.
   //5. 변수값이나 함수를 출력할 때는 {}로 감싸면 됨.
 
-  const hello = "안녕안녕";
   return (
     <>
-      <Hello></Hello>
-      <Bye></Bye>
-      <Greet></Greet>
+      <ExpenseItem></ExpenseItem>
+      <ExpenseItem></ExpenseItem>
     </>
   );
-}
+};
 
 export default App;
