63f24c0e177310a62265ee3273202398789b0da7
hun2zz <tkdgnsdldkdlel@naver.com>
2024-06-13 10:30:58 +0900

    ✅ react study

--- src/components/new-expense/ExpenseForm.js
+++ src/components/new-expense/ExpenseForm.js
@@ -2,13 +2,13 @@ import React, { useState } from "react";
 import "./ExpenseForm.css";
 
 const ExpenseForm = () => {
+  // 입력칸에 있는 3개의 값을 상태값으로 관리
+  const [title, setTitle] = useState("");
+  const [price, setPrice] = useState(0);
+  const [date, setDate] = useState(null);
+
   // 오늘 날짜를 YYYY-MM-DD 형식으로 가져오는 함수
   const getTodayDate = () => {
-    //입력칸에 있는 3개의 값을 상태값으로 관리
-    const [title, setTitle] = useState(title);
-    const [price, setPrice] = useState(0);
-    const [date, setDate] = useState(null);
-
     const today = new Date();
     const year = today.getFullYear();
     const month = String(today.getMonth() + 1).padStart(2, "0"); // 월은 0부터 시작하므로 1을 더해줌
@@ -16,25 +16,27 @@ const ExpenseForm = () => {
     return `${year}-${month}-${day}`;
   };
 
-  //제목이 입력되었을 때 발생하는 이벤트 핸들러
-  const titleChangeHandler = e=> {
-    
-  }
+  // 제목이 입력되었을 때 발생하는 이벤트 핸들러
+  const titleChangeHandler = (e) => {
+    setTitle(e.target.value);
+  };
 
-//가격이 입력되었을 때 발생하는 이벤트 핸들러
-const titleChangeHandler = e=> {
-    
-}
+  // 가격이 입력되었을 때 발생하는 이벤트 핸들러
+  const priceChangeHandler = (e) => {
+    setPrice(e.target.value);
+  };
 
-//날짜가 입력되었을 때 발생하는 이벤트 핸들러
-const titleChangeHandler = e=> {
-    
-}
+  // 날짜가 입력되었을 때 발생하는 이벤트 핸들러
+  const dateChangeHandler = (e) => {
+    setDate(e.target.value);
+  };
+
+  // 폼 전송 이벤트 핸들러
   const submitHandler = (e) => {
-    e.preventDefault();
-    console.log("폼이 전송됨!");
+    e.preventDefault(); // 폼 전송 방지
+    // console.log('폼이 전송됨!');
 
-    //지출 내역 객체를 생성
+    // 지출 내역 객체를 생성
     const newExpense = {
       title,
       price,
