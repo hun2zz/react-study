d9a3fcadf3a277ce346bb46b4abf07f1745086f0
hun2zz <tkdgnsdldkdlel@naver.com>
2024-06-16 23:10:31 +0900

    ✅ course 추가

--- src/components/CourseGoals/CourseItem.js
+++ src/components/CourseGoals/CourseItem.js
@@ -1,7 +1,16 @@
 import React from "react";
 import "./CourseItem.css";
-const CourseItem = ({ item }) => {
-  return <li className="goal-item">{item.text}</li>;
+const CourseItem = ({ item, onDelete }) => {
+  const deleteHandler = (e) => {
+    console.log("삭제해종");
+    onDelete(item.id);
+    // 여기서 App.js 에게 삭제 대상의 id를 전달
+  };
+  return (
+    <li className="goal-item" onClick={deleteHandler}>
+      {item.text}
+    </li>
+  );
 };
 
 export default CourseItem;
