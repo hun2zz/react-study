7c7a5ca35e00aecc02fbb038a5fc58f79bb48ea5
hun2zz <tkdgnsdldkdlel@naver.com>
2024-06-20 11:01:29 +0900

    ✅ stop이 안도니ㅡㄴ 원인

--- src/components/Ref/TimeChallenge.js
+++ src/components/Ref/TimeChallenge.js
@@ -7,12 +7,21 @@ const TimerChallenge = ({ title, targetTime }) => {
   // 타겟 시간이 종료되었는지 여부
   const [timerExpired, setTimerExpired] = useState(false);
 
+  let timer;
   const startHandler = (e) => {
-    setTimeout(() => {
+    timer = setTimeout(() => {
       // console.log("시간만료");
       setTimerExpired(true);
     }, targetTime * 1000);
     setTimerStarted(true);
+  };
+
+  //이 상황엠 stop이 작동핮 ㅣ않는 이유는 start 시의 timer 변수가 지역변수이기 때문에
+  // 상태변수의 setter 호출 시 리렌더링이 되면서 새로운 지역변수로 바뀜
+  // stop 시의 timer와 start 시의 timer는 다른 변수다.
+  const stopHandler = (e) => {
+    console.log("stop");
+    clearTimeout(timer);
   };
   return (
     <section className="challenge">
@@ -22,7 +31,7 @@ const TimerChallenge = ({ title, targetTime }) => {
         {targetTime} second{targetTime > 1 ? "s" : ""}
       </p>
       <p>
-        <button onClick={startHandler}>
+        <button onClick={timerStarted ? stopHandler : startHandler}>
           {timerStarted ? "Stop" : "Start"} Challenge
         </button>
       </p>
