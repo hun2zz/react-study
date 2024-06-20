a373ebad10f99d3f09a16774885ba679e43c76ea
hun2zz <tkdgnsdldkdlel@naver.com>
2024-06-20 10:44:32 +0900

    ✅  도전과제 컴포넌트 생성

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React from "react";
 import Player from "./components/Ref/Player";
+import TimerChallenge from "./components/Ref/TimeChallenge";
 
 const App = () => {
   return (
     <>
       <Player></Player>
-      <div id="challenges"></div>
+      <div id="challenges">
+        <TimerChallenge title="Easy" targetTime={1}></TimerChallenge>
+        <TimerChallenge title="Not Easy" targetTime={5}></TimerChallenge>
+        <TimerChallenge title="Getting Tough" targetTime={10}></TimerChallenge>
+        <TimerChallenge title="Pros only" targetTime={15} />
+      </div>
     </>
   );
 };
