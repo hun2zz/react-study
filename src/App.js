440d60502cf1106b0022a47dbdb8b285f26a451e
hun2zz <tkdgnsdldkdlel@naver.com>
2024-06-20 10:30:17 +0900

    ✅ ref와 상태값 연습 예제

--- src/App.js
+++ src/App.js
@@ -1,40 +1,12 @@
-import React, { useEffect, useState } from "react";
-import "./App.css";
-import TodoTemplate from "./components/Todo/TodoTemplate";
-// bootStrap
-import "bootstrap/dist/css/bootstrap.min.css";
+import React from "react";
+import Player from "./components/Ref/Player";
+
 const App = () => {
-  const todo = [
-    {
-      id: 1,
-      title: "ㄴㅁㅇㅁㄴㅇㅁㄴㅇzzzzㅁㄴㅇ123",
-    },
-    {
-      id: 2,
-      title: "ㄴㅁㅇㅁㄴㅇㅁㄴㅇㅁㄴㅇ12aasdasdsdasd3",
-    },
-  ];
-
-  const [todoList, setTodoList] = useState(todo);
-
-  const onAddExpense = (userInput) => {
-    console.log("App.js 가 내려보낸 함수 호출!");
-    // console.log(userInput);
-    // todoList.push(userInput);
-    setTodoList([...todoList, userInput]);
-    // console.log(expense);
-  };
-
-  const onDeleteExpense = (id) => {
-    setTodoList(todoList.filter((todo) => todo.id !== id));
-  };
-
   return (
-    <TodoTemplate
-      onSave={onAddExpense}
-      onList={todoList}
-      onDelete={onDeleteExpense}
-    ></TodoTemplate>
+    <>
+      <Player></Player>
+      <div id="challenges"></div>
+    </>
   );
 };
 
