074ad5125f5573b2801d25b804b4949822d3d868
hun2zz <tkdgnsdldkdlel@naver.com>
2024-06-20 11:16:01 +0900

    ✅ stop이 안되는 원인 2

--- src/components/Ref/TimeChallenge.js
+++ src/components/Ref/TimeChallenge.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-
+let timer;
 const TimerChallenge = ({ title, targetTime }) => {
   //타이머가 시작되었는지를 확인하는 상태값
   const [timerStarted, setTimerStarted] = useState(false);
@@ -7,7 +7,6 @@ const TimerChallenge = ({ title, targetTime }) => {
   // 타겟 시간이 종료되었는지 여부
   const [timerExpired, setTimerExpired] = useState(false);
 
-  let timer;
   const startHandler = (e) => {
     timer = setTimeout(() => {
       // console.log("시간만료");
@@ -19,6 +18,12 @@ const TimerChallenge = ({ title, targetTime }) => {
   //이 상황엠 stop이 작동핮 ㅣ않는 이유는 start 시의 timer 변수가 지역변수이기 때문에
   // 상태변수의 setter 호출 시 리렌더링이 되면서 새로운 지역변수로 바뀜
   // stop 시의 timer와 start 시의 timer는 다른 변수다.
+
+
+  //전역변수로 timer 설정 시 5초 - > 1초-> -> 1초 -> 5초를 연속클릭하면
+  //5초짜리 timer가 정상작동하지 않는 이유는
+  //4개의 TimeChallenge 컴포넌트가 1개의 timer를 공유하여
+  //처음 5초짜리 timer가 1초짜리 timer에게 덮어씌워지기 때문.
   const stopHandler = (e) => {
     console.log("stop");
     clearTimeout(timer);
