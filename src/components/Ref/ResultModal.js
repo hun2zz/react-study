2f6e85a2c9d766ae7f33d9e0bad3d5d1ee268f70
hun2zz <tkdgnsdldkdlel@naver.com>
2024-06-20 12:22:39 +0900

    ✅ setInterval 처리

--- src/components/Ref/ResultModal.js
+++ src/components/Ref/ResultModal.js
@@ -1,19 +1,15 @@
-import React, { useEffect, useRef } from "react";
+import React, { forwardRef } from "react";
 
-const ResultModal = ({ result, targetTime }) => {
-  const dialog = useRef();
-  useEffect(() => {
-    dialog.current.showModal();
-  }, []);
-
+const ResultModalComponent = ({ result, targetTime, remainingTime }, ref) => {
   return (
-    <dialog ref={dialog} className="result-modal">
-      <h2>Your {result}</h2>
+    <dialog ref={ref} className="result-modal">
+      <h2>Your {result}!</h2>
       <p>
         The target time was <strong>{targetTime} seconds.</strong>
       </p>
       <p>
-        You stopped the timer with <strong>X seconds left.</strong>
+        You stopped the timer with{" "}
+        <strong>{remainingTime} seconds left.</strong>
       </p>
       <form method="dialog">
         <button>Close</button>
@@ -21,5 +17,9 @@ const ResultModal = ({ result, targetTime }) => {
     </dialog>
   );
 };
+
+//forwardRef는 콜백으로 전달된 컴포넌트에 첫 번째 파라미터로 props를 전달하고
+//두번째 파라미터에는 부모에게 받은 ref를 전달한다.
+const ResultModal = forwardRef(ResultModalComponent);
 
 export default ResultModal;
--- src/components/Ref/TimeChallenge.js
+++ src/components/Ref/TimeChallenge.js
@@ -10,34 +10,56 @@ const TimerChallenge = ({ title, targetTime }) => {
 
   // 타겟시간이 종료되었는지 여부
   const [timerExpired, setTimerExpired] = useState(false);
+  //stop을 눌렀을 때 남은 시간을 상태값으로 관리
+  const [timeRemaining, setTimeRemaining] = useState(targetTime * 1000);
+
+  //자식 컴포넌트 ResultModal에 있는 dialog 태그의 참조를 만듬
+  const dialog = useRef();
+
+  //start stop 활성화 조건
+  const timerIsActive = timeRemaining > 0 && timeRemaining < targetTime * 1000;
+
+  //시간이 타임오버 되었을 때 처리
+  if (timeRemaining <= 0) {
+    clearInterval(timer.current);
+    dialog.current.showModal();
+  }
 
   const startHandler = (e) => {
-    timer.current = setTimeout(() => {
-      setTimerExpired(true);
-    }, targetTime * 1000);
+    timer.current = setInterval(() => {
+      setTimeRemaining((prevTime) => prevTime - 10);
+    }, 10);
 
     setTimerStarted(true);
   };
 
   const stopHandler = (e) => {
-    clearTimeout(timer.current);
+    clearInterval(timer.current);
+    dialog.current.showModal();
   };
 
   return (
     <>
-      {<ResultModal targetTime={targetTime} result="lost"></ResultModal>}
+      {
+        <ResultModal
+          ref={dialog}
+          targetTime={targetTime}
+          result="lost"
+          remainingTime={timeRemaining}
+        ></ResultModal>
+      }
       <section className="challenge">
         <h2>{title}</h2>
         <p className="challenge-time">
           {targetTime} second{targetTime > 1 ? "s" : ""}
         </p>
         <p>
-          <button onClick={timerStarted ? stopHandler : startHandler}>
-            {timerStarted ? "Stop" : "Start"} Challenge
+          <button onClick={timerIsActive ? stopHandler : startHandler}>
+            {timerIsActive ? "Stop" : "Start"} Challenge
           </button>
         </p>
-        <p className={timerStarted ? "active" : undefined}>
-          {timerStarted ? "Time is running..." : "Timer inactive"}
+        <p className={timerIsActive ? "active" : undefined}>
+          {timerIsActive ? "Time is running..." : "Timer inactive"}
         </p>
       </section>
     </>
