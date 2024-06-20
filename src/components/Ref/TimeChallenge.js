12a74670fe4a60d235f8e2db47d33b95970bde65
hun2zz <tkdgnsdldkdlel@naver.com>
2024-06-20 10:55:01 +0900

    ✅ 타이머 시작 이벤트 생성

--- src/components/Ref/TimeChallenge.js
+++ src/components/Ref/TimeChallenge.js
@@ -1,16 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 
 const TimerChallenge = ({ title, targetTime }) => {
+  //타이머가 시작되었는지를 확인하는 상태값
+  const [timerStarted, setTimerStarted] = useState(false);
+
+  // 타겟 시간이 종료되었는지 여부
+  const [timerExpired, setTimerExpired] = useState(false);
+
+  const startHandler = (e) => {
+    setTimeout(() => {
+      // console.log("시간만료");
+      setTimerExpired(true);
+    }, targetTime * 1000);
+    setTimerStarted(true);
+  };
   return (
     <section className="challenge">
       <h2>{title}</h2>
+      {timerExpired && <p>You Lost!</p>}
       <p className="challenge-time">
         {targetTime} second{targetTime > 1 ? "s" : ""}
       </p>
       <p>
-        <button>Start Challenge</button>
+        <button onClick={startHandler}>
+          {timerStarted ? "Stop" : "Start"} Challenge
+        </button>
       </p>
-      <p className="">Time is running... / Timer inactive</p>
+      <p className={timerStarted ? "active" : undefined}>
+        {timerStarted ? `Time is running...` : `Timer inactive`}
+      </p>
     </section>
   );
 };
